3fbf38ae0f4cf94289f12fa51755946407bc6997
Salma Elbehairy <salma.elbehairy@gmail.com>
2022-09-21 13:02:00 +0200

    Edit

--- project.js
+++ project.js
@@ -1,9 +1,14 @@
 var time=window.localStorage.getItem('time');
-document.getElementById("sec").innerHTML=time;
-document.getElementById("level").innerHTML=window.localStorage.getItem('level');
+var level=window.localStorage.getItem('level');
+score=document.getElementById('score');
+levelElement=document.getElementById("level");
+timeElement=document.getElementById("sec");
+startPlay=document.getElementById('starting');
+startBtn=document.getElementById('Btnn') ;
+timeElement.innerHTML=time;
+levelElement.innerHTML=level;
 let selectLevel=()=>{ 
-    var level=document.getElementById('choose').value;
-    var lev;
+    level=document.getElementById('choose').value;
     if (level==1){
         window.localStorage.setItem('time','20')
         window.localStorage.setItem('level','Easy')
@@ -16,40 +21,40 @@ let selectLevel=()=>{
         window.localStorage.setItem('level','Hard')
     }
     time=window.localStorage.getItem('time');
-    document.getElementById("sec").innerHTML=time;
-    document.getElementById("level").innerHTML=window.localStorage.getItem('level');
+    timeElement.innerHTML=time;
+    levelElement.innerHTML=window.localStorage.getItem('level');
 }
 
 
 let Generating = () => {
-    var arr=['week' , 'Congratulations','Bee' , 'Location' , 'Shopping' ,'Collage' , 'graduation' , 'Love' , 'Money' , 'suggestions']
+    let arr=['week' , 'Congratulations','Bee' , 'Location' , 'Shopping' ,'Collage' , 'graduation' , 'Love' , 'Money' , 'suggestions' , 'Charger' , 'Bag' , 'Mobile' , 'scarf' , 'TV' , 'Computer' ]
      let ran = Math.ceil(Math.random() * arr.length-1);
     return arr[ran] ; 
 }
 let startGame =()=>{
     time=window.localStorage.getItem('time');
-    document.getElementById('score').innerHTML=0;
+    score.innerHTML=0;
     var Timer=setInterval(function(){
         if(time <= 0){
             clearInterval(Timer);
         }
-        document.getElementById("sec").innerHTML=time;
+        timeElement.innerHTML=time;
         time -=1;
     },1000)
     var word = Generating() ;
-    document.getElementById('starting') . innerHTML = word ;
-    document.getElementById('Btnn') . innerHTML =  `<input type="text" id='text' onkeyup='validate()'>`; 
+    startPlay . innerHTML = word ;
+    startBtn. innerHTML =  `<input type="text" id='text' onkeyup='validate()'>`; 
     setTimeout(function(){
-        document.getElementById('starting') . innerHTML =`Time Out`;
-        document.getElementById('Btnn') . innerHTML =`<button type="button" class="btn btn-lg " onclick="startGame()" >Start again</button>`
+        startPlay. innerHTML =`Time Out`;
+        startBtn. innerHTML =`<button type="button" class="btn btn-lg " onclick="startGame()" >Start again</button>`
     },time*1000)
 }
 let validate=()=>{
     var text=document.getElementById('text').value;
-    word=document.getElementById('starting') . innerHTML;
+    word=startPlay. innerHTML;
     if(text.toLowerCase() == word.toLowerCase()){
         word=Generating();
-        document.getElementById('starting') . innerHTML = word;
+        startPlay. innerHTML = word;
         document.getElementById('text').value='';
         document.getElementById('score') . innerHTML =Number(document.getElementById('score') . innerHTML)+1;
     }
